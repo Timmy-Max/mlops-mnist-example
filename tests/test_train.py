b306b6bd7022f43814266e351aa8b114ae59d596
Timmy-Max <timmy_maks@mail.ru>
2023-10-22 04:04:02 +0300

    fixed tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,34 +1,25 @@
 import torch
 import torch.nn as nn
-from config import (
-    CNN_CONFIG,
-    CNN_OPTIMIZER_CONFIG,
-    CNN_TRAIN_CONFIG,
-    FCN_CONFIG,
-    FCN_OPTIMIZER_CONFIG,
-    FCN_TRAIN_CONFIG,
-)
 
 from mnist_example.datasets import mnist_dataloader
 from mnist_example.models import CNN, FCN
 from mnist_example.train_eval import eval_model, train_model
 
 
-def test_fcn():
+BATCH_SIZE = 50
+LEARNING_RATE = 1e-3
+
+
+def test_fcn() -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    batch_size_fcn = FCN_TRAIN_CONFIG["batch_size"]
     n_epochs_fcn = 1
 
-    train_loader_fcn = mnist_dataloader(
-        batch_size=batch_size_fcn, train=True, shuffle=True
-    )
-    eval_loader_fcn = mnist_dataloader(
-        batch_size=batch_size_fcn, train=False, shuffle=True
-    )
+    train_loader_fcn = mnist_dataloader(batch_size=BATCH_SIZE, train=True, shuffle=True)
+    eval_loader_fcn = mnist_dataloader(batch_size=BATCH_SIZE, train=False, shuffle=True)
 
-    fcn = FCN(**FCN_CONFIG).to(device)
+    fcn = FCN().to(device)
     loss_function = nn.CrossEntropyLoss()
-    optimizer_fcn = torch.optim.Adam(fcn.parameters(), **FCN_OPTIMIZER_CONFIG)
+    optimizer_fcn = torch.optim.Adam(fcn.parameters(), lr=LEARNING_RATE)
 
     init_eval_loss, init_eval_accuracy = eval_model(
         fcn, loss_function, eval_loader_fcn, device
@@ -50,21 +41,16 @@ def test_fcn():
     ), "FCN is not learning"
 
 
-def test_cnn():
+def test_cnn() -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    batch_size_cnn = CNN_TRAIN_CONFIG["batch_size"]
     n_epochs_cnn = 1
 
-    train_loader_cnn = mnist_dataloader(
-        batch_size=batch_size_cnn, train=True, shuffle=True
-    )
-    eval_loader_cnn = mnist_dataloader(
-        batch_size=batch_size_cnn, train=False, shuffle=True
-    )
+    train_loader_cnn = mnist_dataloader(batch_size=BATCH_SIZE, train=True, shuffle=True)
+    eval_loader_cnn = mnist_dataloader(batch_size=BATCH_SIZE, train=False, shuffle=True)
 
-    cnn = CNN(**CNN_CONFIG).to(device)
+    cnn = CNN().to(device)
     loss_function = nn.CrossEntropyLoss()
-    optimizer_cnn = torch.optim.Adam(cnn.parameters(), **CNN_OPTIMIZER_CONFIG)
+    optimizer_cnn = torch.optim.Adam(cnn.parameters(), lr=LEARNING_RATE)
 
     init_eval_loss, init_eval_accuracy = eval_model(
         cnn, loss_function, eval_loader_cnn, device
