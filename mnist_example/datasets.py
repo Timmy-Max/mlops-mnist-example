97f971b596b20fee343f6b6f47b3d9135876d125
Timmy-Max <maksimpotorochin@yandex.ru>
2023-12-01 14:06:35 +0300

    added downloading data from dvc

--- mnist_example/datasets.py
+++ mnist_example/datasets.py
@@ -1,6 +1,23 @@
+import os
+
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from dvc.fs import DVCFileSystem
 from torch.utils.data import DataLoader
+
+
+def check_files():
+    files_paths = [
+        "data/MNIST/raw/t10k-images-idx3-ubyte",
+        "data/MNIST/raw/t10k-labels-idx1-ubyte",
+        "data/MNIST/raw/train-images-idx3-ubyte",
+        "data/MNIST/raw/train-labels-idx1-ubyte",
+    ]
+    result = True
+    for path in files_paths:
+        result &= os.path.exists(path)
+
+    return result
 
 
 def mnist_dataloader(batch_size: int, train: bool, shuffle: bool = True) -> DataLoader:
@@ -14,6 +31,10 @@ def mnist_dataloader(batch_size: int, train: bool, shuffle: bool = True) -> Data
     Returns:
         train or test dataloader
     """
+    if not check_files():
+        fs = DVCFileSystem()
+        fs.get("data", "data", recursive=True)
+
     transform = transforms.Compose(
         [
             transforms.ToTensor(),
@@ -21,7 +42,7 @@ def mnist_dataloader(batch_size: int, train: bool, shuffle: bool = True) -> Data
         ]
     )
     dataset = datasets.MNIST(
-        root="./data", train=train, download=True, transform=transform
+        root="./data", train=train, download=False, transform=transform
     )
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
     return dataloader
